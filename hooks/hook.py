7563a080777a73fd96e52523ce64b03723f7155d
PC <66687936+fischpo@users.noreply.github.com>
2025-01-10 18:49:00 +0530

    Update hook.py

--- hooks/hook.py
+++ hooks/hook.py
@@ -61,10 +61,11 @@ try:
 			requestdata = requests.post(url, json={'query': query, 'variables': variables}).json()
 			for show in requestdata['data']['Page']['mediaList']:
 				nam=[show['media']['title']['romaji'],show['media']['title']['english']]
-				if nam not in showlist:
+				if nam[0] not in showlist['romaji']:
 					showlist['romajii'].append(rawshowtitle(nam[0].upper()))
 					showlist['eng'].append(rawshowtitle(nam[1].upper()))
 					showlist['romaji'].append(nam[0])
+			
 					
 		idexclude=showlist
 except Exception as ee:
