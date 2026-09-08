2e834759f79afeaf7a79a8b4878e0100332561e9
Mura <66687936+fischpo@users.noreply.github.com>
2026-09-08 20:03:34 +0530

    Update hook.py

--- hooks/hook.py
+++ hooks/hook.py
@@ -99,19 +99,13 @@ def new():
     # time.sleep(10)
     # pageviewer.refresh()
     # rawcontent=pageviewer.page_source;print(rawcontent)
-    scraper = cloudscraper.create_scraper(
-    browser={
-        'browser': 'chrome',
-        'platform': 'windows',
-        'desktop': True
-    }
-    )
-    headers = {
-    'Referer': 'https://animepahe.pw',
-    'Accept-Language': 'en-US,en;q=0.9',
-    }
-    response = scraper.get(link, headers=headers)
-    rawcontent=response.text;print(rawcontent)
+    with SB(uc=True, xvfb=True) as sb:
+        sb.driver.uc_open_with_reconnect('https://animepahe.pw', 6)
+        time.sleep(3)
+        sb.uc_gui_click_captcha()
+        sb.driver.uc_open_with_reconnect(link, 6)
+        time.sleep(3)
+        rawcontent = sb.driver.page_source;print(rawcontent)
     jsoncontent = re.findall("<pre>(.*?)</pre>", rawcontent, re.DOTALL)[0]
     response = json.loads(jsoncontent)
     allshowsreleased = [
